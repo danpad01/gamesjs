0d1b2cef60365520837086f6f34430673a087a08
Dani <danpad01@ucm.es>
2020-04-21 00:31:59 +0200

    Apartado 12 completado

---
+++ 3. Introduccion a estructuras mas complejas/12. Bucle principal del juego/js/juego.js
@@ -0,0 +1,22 @@
+var fps = 10;
+var xEscenario = 0;
+
+function atacar(){
+    console.log("Has atacado");
+}
+
+
+function mueveEscenario(){
+    xEscenario++;
+    console.log(xEscenario);
+}
+
+
+//BUCLE PRINCIPAL
+function principal(){
+    mueveEscenario();
+}
+
+
+
+setInterval(principal, 1000/fps);
